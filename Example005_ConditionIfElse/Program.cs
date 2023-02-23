6f7b2114c975e9b7517e6d70a3d76f089671b39b
KsenofontovPavel <kpo89@inbox.ru>
2023-02-23 17:19:32 +0300

    Add Example005

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,17 @@
+﻿//Console.Write("Введите Ваше имя");
+//string username = Console.ReadLine();
+//Console.Write("Привет, ");
+//Console.Write(username);
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
